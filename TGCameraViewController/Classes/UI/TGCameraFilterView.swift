24f6371ff31ba00013df6246d07ad0c49ab1b53f
Angel Cortez <acort3255@gmail.com>
2016-07-21 08:32:27 -0400

    Ported TGCameraFilterView to Swift

---
+++ TGCameraViewController/Classes/UI/TGCameraFilterView.swift
@@ -0,0 +1,62 @@
+//
+//  TGCameraFilterView.swift
+//  TGCameraViewController
+//
+//  Created by Angel Cortez on 7/21/16.
+//  Copyright © 2016 Tudo Gostoso Internet. All rights reserved.
+//
+
+import UIKit
+
+class TGCameraFilterView: UIView {
+    
+    convenience init() {
+        self.init()
+        self.setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.setup()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.setup()
+    }
+    
+    func addToView(view: UIView, aboveView: UIView) {
+        var frame: CGRect = self.frame
+        frame.origin.y = CGRectGetMaxY(view.frame) - CGRectGetHeight(aboveView.frame)
+        self.frame = frame
+        view.addSubview(self)
+        frame.origin.y -= CGRectGetHeight(self.frame)
+        UIView.animateWithDuration(0.5, animations: {() -> Void in
+            self.frame = frame
+            }, completion: {(finished: Bool) -> Void in
+        })
+
+    }
+    
+    func removeFromSuperviewAnimated() {
+        var frame: CGRect = self.frame
+        frame.origin.y += CGRectGetHeight(self.frame)
+        UIView.animateWithDuration(0.5, animations: {() -> Void in
+            self.frame = frame
+            }, completion: {(finished: Bool) -> Void in
+                self.removeFromSuperview()
+        })
+
+    }
+    
+    // MARK: - Private methods
+    
+    
+    func setup() {
+        var frame: CGRect = self.frame
+        frame.size.width = CGRectGetWidth(UIScreen.mainScreen().applicationFrame)
+        self.frame = frame
+    }
+}
