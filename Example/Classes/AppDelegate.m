34fa7943adf2411cdf26ffc80c99c96c13822cea
Angel Cortez <acort3255@gmail.com>
2016-07-27 16:06:23 -0400

    Framework fix

--- Example/Classes/AppDelegate.m
+++ Example/Classes/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "TGCameraViewController.h"
 
 @interface AppDelegate ()
 
--- Example/Classes/Control/TGInitialViewController.h
+++ Example/Classes/Control/TGInitialViewController.h
@@ -7,6 +7,7 @@
 //
 
 @import UIKit;
+@import TGCameraViewController;
 
 @interface TGInitialViewController : UIViewController
 
