732ffb894ad99b81d63848fafeefc4bf6f686433
Angel Cortez <acort3255@gmail.com>
2016-07-27 16:00:11 -0400

    added frameworks

---
+++ TGCameraViewController/TGCameraViewController.h
@@ -0,0 +1,19 @@
+//
+//  TGCameraViewController.h
+//  TGCameraViewController
+//
+//  Created by Angel Cortez on 7/27/16.
+//  Copyright © 2016 Tudo Gostoso Internet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TGCameraViewController.
+FOUNDATION_EXPORT double TGCameraViewControllerVersionNumber;
+
+//! Project version string for TGCameraViewController.
+FOUNDATION_EXPORT const unsigned char TGCameraViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TGCameraViewController/PublicHeader.h>
+
+
